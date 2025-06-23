ec04af17916ef0402e41b0c9c0eb7f411b0e9ef0
Abdelrhman Amr <abdelrhman.elsayed98@eng-st.cu.edu.eg>
2025-06-23 20:28:15 +0300

    Frontend API Paths

---
+++ frontend/finances-tracker/src/utils/apiPath.js
@@ -0,0 +1,24 @@
+export const BASE_URL = "http://localhost:8000";
+
+// utils/apiPath.js
+export const API_PATHS = {
+    AUTH: {
+        LOGIN: "api/v1/auth/login",
+        SIGNUP: "api/v1/auth/register",
+        GET_USER_INFO: "api/v1/auth/getUser",
+    },
+    DASHBOARD: {
+        GET_DATA: "api/v1/dashboard",
+    },
+    INCOME: {
+        GET_ALL: "api/v1/income",
+        ADD: "api/v1/income/add",
+        DELETE: "api/v1/income/delete",
+    },
+    EXPENSE: {
+        GET_ALL: "api/v1/expense",
+        ADD: "api/v1/expense/add",
+        DELETE: "api/v1/expense/delete",
+    },
+    
+};  
