3a54a406a5babd266d2ce5c9ee93ea6cb38d79c1
Abdelrhman Amr <abdelrhman.elsayed98@eng-st.cu.edu.eg>
2025-06-25 01:06:01 +0300

    Fixed Logout

--- frontend/finances-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/finances-tracker/src/components/layouts/SideMenu.jsx
@@ -8,7 +8,7 @@ const SideMenu = ({ activeMenu }) => {
   const navigate = useNavigate();
 
   const handleClick = (route) => {
-    if (route === "logout") {
+    if (route === "/logout") {
       handleLogout();
       return;
     }
@@ -16,9 +16,9 @@ const SideMenu = ({ activeMenu }) => {
   };
 
   const handleLogout = () => {
-    localStorage.clear();
+    localStorage.removeItem("token");
     clearUser();
-    navigate("/login");
+    navigate("/login", { replace: true });
   };
   return (
     <div className="w-64 h-[calc(100vh - 64px)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px] z-20">
@@ -29,7 +29,7 @@ const SideMenu = ({ activeMenu }) => {
         <button
           key={`menu_${index}`}
           className={`w-full flex items-center gap-4 text-[15px] ${
-            activeMenu == item.label ? "text-white bg-primary"  : ""
+            activeMenu == item.label ? "text-white bg-primary" : ""
           } py-3 px-6 rounded-lg mb-3`}
           onClick={() => handleClick(item.path)}
         >
