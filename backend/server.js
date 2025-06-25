38ed4e166bcda2cb27f2b30d455b920288c93e2b
Abdelrhman Amr <abdelrhman.elsayed98@eng-st.cu.edu.eg>
2025-06-25 16:53:18 +0300

    vercel

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,6 @@ app.use('/api/v1/convert', convertRoutes);
 
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); 
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); 
+module.exports = app;
