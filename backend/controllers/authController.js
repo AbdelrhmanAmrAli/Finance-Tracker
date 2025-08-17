66dda05fdb1b9188fa4986e427c6d9dba4899cc6
Abdelrhman Amr <abdelrhman.elsayed98@eng-st.cu.edu.eg>
2025-08-17 15:24:14 +0300

    session lasts for more time now

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 //generate JWT token
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '10m' });
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 }
 
 
